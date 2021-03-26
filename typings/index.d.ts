d19de6d51426b2f181c6664854c928b9e674516f
カグチ <59391215+KagChi@users.noreply.github.com>
2021-03-26 16:53:50 +0700

    Update index.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -9,6 +9,12 @@ declare enum ChannelType {
   unknown = 7,
 }
 
+declare enum StickerFormatTypes {
+  PNG = 1,
+  APNG = 2,
+  LOTTIE = 3,
+}
+
 declare enum OverwriteTypes {
   role = 0,
   member = 1,
@@ -525,6 +531,7 @@ declare module 'discord.js' {
     SystemMessageTypes: SystemMessageType[];
     ActivityTypes: ActivityType[];
     OverwriteTypes: OverwriteTypes;
+    StickerFormatTypes: StickerFormatTypes;
     ExplicitContentFilterLevels: ExplicitContentFilterLevel[];
     DefaultMessageNotifications: DefaultMessageNotifications[];
     VerificationLevels: VerificationLevel[];
@@ -2204,6 +2211,7 @@ declare module 'discord.js' {
     INVITE_ACCEPTED_TO_GUILD_NOT_CONTAINING_BOT: 50036;
     INVALID_API_VERSION: 50041;
     CANNOT_DELETE_COMMUNITY_REQUIRED_CHANNEL: 50074;
+    INVALID_STICKER_SENT: 50081;
     REACTION_BLOCKED: 90001;
     RESOURCE_OVERLOADED: 130000;
   }
@@ -3250,6 +3258,21 @@ declare module 'discord.js' {
   }
 
   type Status = number;
+  
+  export class Sticker extends Base {
+    constructor(client: Client, data: object);
+    public asset: string;
+    public readonly createdTimestamp: number;
+    public readonly createdAt: Date;
+    public description: string;
+    public format: StickerFormatTypes;
+    public id: Snowflake;
+    public name: string;
+    public packID: Snowflake;
+    public previewAsset: string | null;
+    public tags: string[];
+    public readonly url: string;
+  }
 
   interface StreamOptions {
     type?: StreamType;
